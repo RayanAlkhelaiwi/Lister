65c7014ec99c690ff8051ab33518861ecc2c81d1
RayanAlkhelaiwi <r.n.m.k@live.com>
2021-08-26 15:27:26 +0300

    Add root and about routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'home#index'
   get 'home/index'
+  get 'home/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
