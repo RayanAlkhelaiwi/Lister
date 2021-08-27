83e84cac7a42a4f160563d9df3dd6f4bc77e64d7
RayanAlkhelaiwi <r.n.m.k@live.com>
2021-08-27 23:02:50 +0300

    Add user management
    Using devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :lists
   root 'lists#index'
   get 'home/index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_26_124501) do
+ActiveRecord::Schema.define(version: 2021_08_27_191858) do
 
   create_table "lists", force: :cascade do |t|
     t.string "item"
@@ -19,6 +19,18 @@ ActiveRecord::Schema.define(version: 2021_08_26_124501) do
     t.datetime "timestamp"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
