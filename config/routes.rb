5c89cb5072207c7c22a9dfa6b664787cecc0daac
RayanAlkhelaiwi <r.n.m.k@live.com>
2021-08-30 16:24:15 +0300

    Remove extra page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :lists
   root 'lists#index'
-  get 'home/index'
   get 'home/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
